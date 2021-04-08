411e2489c0a4d4fbd89d1816f561c14e772b3bf9
Ayman Osman <aymano.osman@gmail.com>
2021-04-08 05:48:22 +0100

    init

---
+++ find-requires.rkt
@@ -0,0 +1,31 @@
+#lang racket/base
+
+(provide find-requires)
+
+(require racket/list
+         syntax/parse)
+
+(define (require-form? stx)
+  (syntax-parse stx
+    #:literals (#%require)
+    [(#%require mod-path)
+     #'mod-path]
+    [_
+     #f]))
+
+(define (require-forms path)
+  (call-with-input-file path
+    (lambda (in)
+      (define stx
+        (parameterize ([read-accept-reader #t])
+          (parameterize ([current-namespace (make-base-namespace)])
+            (expand (read-syntax path (open-input-file path))))))
+
+      (syntax-parse stx
+        #:literals (module)
+        [(module name lang (#%module-begin form* ...))
+         (filter-map require-form? (syntax->list #'(form* ...)))]))))
+
+(define (find-requires path)
+  (for/hash ([stx (require-forms path)])
+    (values (symbol->string (syntax-e stx)) stx)))
---
+++ unused-requires.rkt
@@ -0,0 +1,34 @@
+#lang racket/base
+
+(provide main
+         display-unused-requires)
+
+(require racket/match
+         racket/cmdline
+         "find-requires.rkt")
+
+(require macro-debugger/analysis/private/util
+         macro-debugger/analysis/private/nom-use-alg
+         macro-debugger/analysis/private/get-references)
+
+;; Copied from macro-debugger package
+(define (analyze-requires mod-path)
+  (let-values ([(compiled deriv) (get-module-code/trace mod-path)])
+    (nom-use-alg (deriv->refs deriv) compiled)))
+
+(define (display-unused-requires mod-path)
+  (define require=>syntax (find-requires mod-path))
+  (for ([entry (in-list (analyze-requires mod-path))])
+    (match entry
+      [(list 'drop mpi phase)
+       (define key (symbol->string (mpi->key mpi)))
+       (define stx (hash-ref require=>syntax key))
+       (printf "~a ~a\n" key (syntax-position stx))]
+      [_
+       (void)])))
+
+(define (main . args)
+  (command-line
+   #:program "intellij/unused-requires"
+   #:args (path)
+   (display-unused-requires path)))
