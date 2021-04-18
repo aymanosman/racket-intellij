2d066acb1ecc0e04be555796e5428b6236b6c0ba
Ayman Osman <aymano.osman@gmail.com>
2021-04-18 20:24:11 +0100

    handle missing module exception

--- analysis/lint-with-expand.rkt
+++ analysis/lint-with-expand.rkt
@@ -13,7 +13,7 @@
                        [(empty? exprs)
                         (void)]
                        [else
-                        (write-lint-message (first exprs) "unbound identifier")]))]
+                        (write-lint-message/syntax (first exprs) "unbound identifier")]))]
                   [exn:fail:syntax?
                    (lambda (exn)
                      (define exprs (exn:fail:syntax-exprs exn))
@@ -23,18 +23,26 @@
                        [else
                         (cond
                           [(string-contains? (exn-message exn) "duplicate binding")
-                           (write-lint-message (first exprs) "duplicate binding")]
+                           (write-lint-message/syntax (first exprs) "duplicate binding")]
                           [(string-contains? (exn-message exn) "bad syntax")
-                           (write-lint-message (first exprs) "bad syntax")]
+                           (write-lint-message/syntax (first exprs) "bad syntax")]
                           [else
-                           (write-lint-message (first exprs) "error detected")])]))]
+                           (write-lint-message/syntax (first exprs) "error detected")])]))]
+                  [exn:fail:filesystem:missing-module?
+                   (lambda (exn)
+                     (define path (exn:fail:filesystem:missing-module-path exn))
+                     (write-lint-message 0 (format "cannot open module file ~a" (path->string path))))]
                   [exn:fail?
                    (lambda (exn)
+                     (println exn)
                      (eprintf "~a" (exn-message exn)))])
     (parameterize ([read-accept-reader #t])
       (parameterize ([current-namespace (make-base-namespace)])
         (expand (read-syntax file-name in))))
     (void)))
 
-(define (write-lint-message stx message)
-  (printf "~a ~s ~a\n" (syntax-position stx) message (syntax->datum stx)))
+(define (write-lint-message/syntax stx message)
+  (write-lint-message (syntax-position stx) message (syntax->datum stx)))
+
+(define (write-lint-message offset message [detail ""])
+  (printf "~s ~s ~a\n" offset message detail))
